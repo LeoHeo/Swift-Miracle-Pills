8be19c90bbacb556a44c5cd928b6432658e7970f
LeoHeo <hjh5488@gmail.com>
2016-10-03 17:37:43 +0900

    update mainView: delete statePickerView, add state, country, zipcode textfield
    기존에 UIPickerView를 없애고 state, country, zipcode TextField 추가 및
    buyNow Button 추가
    
    buyNodw 클릭시 Sucess라는 이미지 있는 ViewController보이게 Trigger
    Segues

--- Miracle Pills/ViewController.swift
+++ Miracle Pills/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ViewController.swift`
 //  Miracle Pills
 //
 //  Created by JinHan on 2016. 9. 25..
@@ -8,63 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-    
-    /*------------------------------------
-     UIPickerViewDelegate는 UIPickerView에서
-     스크롤링, 선택 같은 행위를 할때 불러올수 있는 함수들이 있음
-    --------------------------------------*/
-    
-    // weak는 약함 참조
-    @IBOutlet weak var statePicker: UIPickerView!
-    @IBOutlet weak var statePickerBtn: UIButton!
-    
-    let states = ["Alaska", "Arkansas", "Alabama", "California", "Maine", "New York"]
+class ViewController: UIViewController{
 
     override func viewDidLoad(){
         super.viewDidLoad()
-        self.statePicker.dataSource = self //
-        self.statePicker.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    @IBAction func stateBtnPressed(_ sender: UIButton) {
-        statePicker.isHidden = false
-    }
-    
-    
-    /*-----------------------------
-        필수 정의 함수 2개
-        DataSource 함수 2개
-        - numberOfComponents
-        - pickerView(numberOfRowsInComponent)
-     -----------------------*/
-    
-    // How may Components
-    // 화면에 UIPickerView가 몇개가 나올것이냐
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    // 화면에 뿌릴 갯수
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return states.count
-    }
-    
-    // UIPickerView Display name
-    // 이 함수가 없을 경우 ?로 도배된 UIPickerView가 나온다
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return states[row]
-    }
-    
-    // UIPickerView Select
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        statePickerBtn.setTitle(states[row], for: UIControlState())
-        statePicker.isHidden = true
+        // Dispose of any resources that can be recreated.`
     }
 }
 
